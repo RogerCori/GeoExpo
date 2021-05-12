5e6e98dcad5661334c046e71fc68a37b5388be2b
Javier YucraZ <yucrazj@gmail.com>
2021-05-11 21:16:02 -0400

    Nuevas Dependencias

---
+++ screens/Main.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import * as Location from "expo-location";
+import { Appbar, Text } from "react-native-paper";
+import MapPage from "./Map";
+
+const MainPage = () => {
+  const [permisoGeo, setPermisoGeo] = useState(null);
+  const [latitud, setLatitud] = useState(0);
+  const [longitud, setLongitud] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      setPermisoGeo(status === "granted");
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLatitud(location.coords.latitude);
+      setLongitud(location.coords.longitude);
+    })();
+  }, []);
+
+  if (!permisoGeo) {
+    return <Text>Esperando permisos de Geolocalizacion</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Appbar.Header
+        style={{
+          width: Dimensions.get("window").width,
+          backgroundColor: "#abc",
+          borderTopColor : "black",
+          borderTopWidth : 1,
+          borderStyle : "solid"
+        }}
+      >
+        <Appbar.BackAction onPress={() => {}} />
+        <Text>Algo</Text>
+      </Appbar.Header>
+      <MapPage latitud={latitud} longitud={longitud}></MapPage>
+    </View>
+  );
+};
+
+export default MainPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
